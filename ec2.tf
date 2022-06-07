b5620920fe46086dfd390d20dd1a8eb294bd8f91
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-06-07 18:54:00 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "myec2" {
+  ami                    = "ami-05ba3a39a75be1ec4"
+  instance_type          = "t2.micro"
+  availability_zone = "ap-south-1a"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = "MyNewKey"
+
+  tags = {
+    name = "testec2"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.myec2.public_ip} >> publicIP.txt"
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,27 @@
+//Security group creation and whitelisting the ip
+resource "aws_security_group" "allow_tls" {
+  name = "terraform-sg"
+
+  ingress {
+    description = "Allow port 22 - inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 80 - inbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
